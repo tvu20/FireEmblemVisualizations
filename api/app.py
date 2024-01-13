8ea3ad741e03e05e8dbdc227ab42cb3c4195fc63
Giao Vu Dinh <tvudinh20@gmail.com>
2024-01-13 23:19:01 +0800

    testing the predictor
    example predictor
    
    changed gitignore
    
    removing large files fromcommit

--- api/app.py
+++ api/app.py
@@ -1,6 +1,12 @@
 import time
-from flask import (Flask, send_from_directory)
+from flask import (
+    Flask,
+    send_from_directory,
+    request
+)
 from flask_cors import CORS
+
+import predictor as prd
 
 app = Flask(__name__, 
             static_url_path='',
@@ -27,9 +33,34 @@ def not_found(e):
 # ---------------------------------------------
 
 @app.route('/api/time')
-def get():
+def get_time():
     return {'time': time.time()}
 
-# @app.errorhandler(404)
-# def not_found(e):
-#     return render_template("index.html")
+
+# -----
+# testing the predictors
+
+# predictor = ktrain.load_predictor('models/my_model.keras')
+
+# def prediction(text):
+#     result = predictor.predict(text)
+
+#     return result
+
+@app.route('/api/predict-emotion')
+def get_prediction():
+    sentence = request.args.get('sentence')
+    sentence = sentence.replace("%20", " ")
+    print(sentence)
+
+    label = prd.prediction(sentence)
+
+    return {'label': label}
+
+# -----
+
+
+# ----------------------------------------------------------------------
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
