875ba920b90a89e00201b43d9e69ce99aeb067ff
Giao Vu Dinh <tvudinh20@gmail.com>
2024-01-30 10:47:23 -0500

    navigation

---
+++ frontend/src/pages/ErrorPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import NavBar from "../components/NavBar";
+
+function ErrorPage() {
+  return (
+    <>
+      {/* <NavBar /> */}
+      <div className="body__page-content">
+        <h1>Oops! This page doesn't exist.</h1>
+      </div>
+    </>
+  );
+}
+
+export default ErrorPage;
---
+++ frontend/src/routes.jsx
@@ -0,0 +1,42 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import Wrapper from "./components/Wrapper";
+import ErrorPage from "./pages/ErrorPage";
+import HomePage from "./pages/home/HomePage";
+import AboutPage from "./pages/about/AboutPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Wrapper />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/about",
+        element: <AboutPage />,
+      },
+      //   {
+      //     path: "/les-mis",
+      //     element: <LesMis />,
+      //   },
+      //   {
+      //     path: "/social-graphs",
+      //     element: <SocialGraphs />,
+      //   },
+      //   {
+      //     path: "/inputs",
+      //     element: <InputsPage />,
+      //   },
+      //   {
+      //     path: "/outputs",
+      //     element: <OutputsPage />,
+      //   },
+    ],
+  },
+]);
+
+export default router;
