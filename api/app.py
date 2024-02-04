b4a04de03d08ba10acd40ab0bbe230af0062157a
Giao Vu Dinh <tvudinh20@gmail.com>
2024-02-04 11:35:27 -0500

    api updates

--- api/app.py
+++ api/app.py
@@ -110,3 +110,49 @@ def get_pairing_games():
     response = json.load(f)
 
     return list(response.keys())
+
+# ---------------------------------------------
+# Routes for scripts article
+# ---------------------------------------------
+
+@app.route('/api/scripts/word-counts')
+def get_word_counts():
+    f = open(os.path.join(app.root_path, "script", "word_counts.json"), "r")
+    response = json.load(f)
+
+    return response
+
+@app.route('/api/scripts/words-per-chapter')
+def get_words_per_chapter():
+    f = open(os.path.join(app.root_path, "script", "words_per_chapter.json"), "r")
+    response = json.load(f)
+
+    return response
+
+@app.route('/api/scripts/unique-words')
+def get_unique_words():
+    f = open(os.path.join(app.root_path, "script", "unique_words.json"), "r")
+    response = json.load(f)
+
+    return response
+
+@app.route('/api/scripts/words-once')
+def get_words_once():
+    f = open(os.path.join(app.root_path, "script", "words_once.json"), "r")
+    response = json.load(f)
+
+    return response
+
+@app.route('/api/scripts/longest-words')
+def get_longest_words():
+    f = open(os.path.join(app.root_path, "script", "longest_words.json"), "r")
+    response = json.load(f)
+
+    return response
+
+@app.route('/api/scripts/common-words')
+def get_common_words():
+    f = open(os.path.join(app.root_path, "script", "most_common_words.json"), "r")
+    response = json.load(f)
+
+    return response
