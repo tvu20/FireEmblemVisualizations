b58f75182da0889fd12568b33ada6f4e9dbf4063
Giao Vu Dinh <tvudinh20@gmail.com>
2024-02-08 15:17:11 -0500

    supports network customization!

--- frontend/src/pages/viz/relationships/SupportNetworksPage.jsx
+++ frontend/src/pages/viz/relationships/SupportNetworksPage.jsx
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 import VizWrapper from "../../../components/visualizations/VizWrapper";
-import SupportsFE16 from "../../../visualizations/relationships/supports/SupportsFE16";
+import Supports from "../../../visualizations/relationships/supports/Supports";
 
 import getColor from "../../../utils/colors";
 
 import "./relationships.css";
 
 function SupportNetworksPage() {
+  const [game, setGame] = useState("FE6");
+  const [linkConstrain, setLinkConstrain] = useState(false);
   return (
     <VizWrapper color={getColor("relationships")} navColor={"white"}>
       <div className="support-networks">
         <h1>Support Networks</h1>
         <p>Some description here.</p>
-        <SupportsFE16 />
+        <input
+          type="checkbox"
+          checked={linkConstrain}
+          onChange={(e) => setLinkConstrain((prevState) => !prevState)}
+        />
+        <select
+          value={game}
+          onChange={(e) => setGame(e.target.value)}
+          style={{ marginBottom: "30px" }}
+        >
+          <option value="FE6">The Binding Blade</option>
+          <option value="FE7">The Blazing Blade</option>
+          <option value="FE8">The Sacred Stones</option>
+          <option value="FE9">Path of Radiance</option>
+          <option value="FE13">Awakening</option>
+          <option value="FE14">Fates</option>
+          <option value="FE15">Echoes</option>
+          <option value="FE16">Three Houses</option>
+        </select>
+        <Supports
+          game={game}
+          constrain={game === "FE15" || game === "FE6" ? true : false}
+          linkConstrain={linkConstrain}
+        />
       </div>
     </VizWrapper>
   );
