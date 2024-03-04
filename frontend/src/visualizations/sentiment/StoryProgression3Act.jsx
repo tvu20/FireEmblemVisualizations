52d6e487cdb3cab464bea3ed55dddac5142991d0
Giao Vu Dinh <tvudinh20@gmail.com>
2024-03-04 00:29:15 -0500

    story progression updates!

--- frontend/src/visualizations/sentiment/StoryProgression3Act.jsx
+++ frontend/src/visualizations/sentiment/StoryProgression3Act.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import * as d3 from "d3";
 
-import useWindowDimensions from "../../hooks/useWindowDimensions";
+import useWindowDimensions from "../../hooks/useWindowDimensions.jsx";
 import { storyIcons } from "../../utils/story.js";
 // import { getGameTitles } from "../../utils/games";
 
@@ -11,10 +11,8 @@ const STAGE_COLORS = {
   setup: "#e65507",
   pursuit: "#e3b027",
   trials: "#389c2a",
-  darkness: "#175234",
   journey: "#6e441f",
   finale: "#b5046b",
-  rebuild: "#2f2266",
 };
 
 function StoryProgression(props) {
@@ -28,15 +26,12 @@ function StoryProgression(props) {
     d3.selectAll(".story-progression-container > svg").remove();
 
     var margin = { top: 30, right: 10, bottom: 0, left: 10 },
-      width = 800 - margin.left - margin.right;
+      width = 800 - margin.left - margin.right,
+      height = 140 - margin.top - margin.bottom;
 
     const div = d3.select(ref.current);
 
-    Object.entries(data).map(([key, value], index) => {
-      const height = value.guide
-        ? 160 - margin.top - margin.bottom
-        : 140 - margin.top - margin.bottom;
-
+    Object.entries(data).map(([key, value]) => {
       const svg = div
         .append("svg")
         .attr("width", width + margin.left + margin.right)
@@ -55,26 +50,6 @@ function StoryProgression(props) {
       const act_height = 30;
       const stage_height = 40;
       const moments_height = 85;
-
-      if (value.guide) {
-        svg
-          .append("line")
-          .attr("x1", 0)
-          .attr("x2", width)
-          .attr("y1", 120)
-          .attr("y2", 120)
-          .style("stroke", "#333")
-          .style("stroke-width", "1px");
-
-        svg
-          .append("rect")
-          .attr("x", -10)
-          .attr("y", -20)
-          .attr("width", width + 20)
-          .attr("height", 140)
-          .style("fill", "white")
-          .style("opacity", 0.3);
-      }
 
       svg
         .append("text")
@@ -164,57 +139,24 @@ function StoryProgression(props) {
           "translate(" + x(value.midpoint) + "," + act_height + ")"
         );
 
-      if (value.group === 4) {
-        g.append("rect")
-          .attr("x", -69)
-          .attr("y", -16)
-          .attr("width", 70)
-          .attr("height", 20)
-          .style("fill", "black");
+      g.append("rect")
+        .attr("x", -34)
+        .attr("y", -16)
+        .attr("width", 70)
+        .attr("height", 20)
+        .style("fill", "black");
 
-        g.append("rect")
-          .attr("x", -1)
-          .attr("y", 0)
-          .attr("width", 2)
-          .attr("height", 10);
-        g.append("text")
-          .attr("transform", "translate(-66,0)")
-          .text("MIDPOINT")
-          .style("fill", "white")
-          .style("font-size", "13px");
+      g.append("rect")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("width", 2)
+        .attr("height", 10);
 
-        g.append("rect")
-          .attr("x", 1)
-          .attr("y", -16)
-          .attr("width", 40)
-          .attr("height", 20)
-          .style("fill", "gray");
-
-        g.append("text")
-          .attr("transform", "translate(3,0)")
-          .text("Act III")
-          .style("fill", "white")
-          .style("font-size", "13px");
-      } else {
-        g.append("rect")
-          .attr("x", -34)
-          .attr("y", -16)
-          .attr("width", 70)
-          .attr("height", 20)
-          .style("fill", "black");
-
-        g.append("rect")
-          .attr("x", 0)
-          .attr("y", 0)
-          .attr("width", 2)
-          .attr("height", 10);
-
-        g.append("text")
-          .attr("transform", "translate(-31,0)")
-          .text("MIDPOINT")
-          .style("fill", "white")
-          .style("font-size", "13px");
-      }
+      g.append("text")
+        .attr("transform", "translate(-31,0)")
+        .text("MIDPOINT")
+        .style("fill", "white")
+        .style("font-size", "13px");
     });
   }, [data, windowWidth]);
 
